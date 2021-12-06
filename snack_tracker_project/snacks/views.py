c77bf4031a3a9e2c5a9babb565c2902ef4dee5f0
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-12-06 15:01:56 +0200

    finish

--- snack_tracker_project/snacks/views.py
+++ snack_tracker_project/snacks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Snack
 
-# Create your views here.
+# Create your views here .
+
+class SnackListView(ListView) :
+    template_name = 'snack_list.html'
+    model = Snack
+
+class SnackDateView(DetailView) :
+    template_name = 'snack_detail.html'
+    model = Snack
